a357078807daa868538373912ed7e583af59a80d
zmiycheg <zmiycheg@gmail.com>
2022-08-19 16:21:50 +0300

    add hide question, add user field validation

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
 
   root to: 'questions#index'
   resource :session, only: %i[new create destroy]
-  resources :questions
+  resources :questions do
+    member do
+      put :hide
+      patch :hide
+    end
+  end
   resources :users, only: %i[new create update edit destroy]
 
 end
