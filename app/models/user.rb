447a95b758cc4b874e25dfd28efbda4eb2ada396
Max Gorelov <zmiycheg@gmail.com>
2022-08-26 17:39:50 +0300

    header color validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
     format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
   validates :nickname, length: { maximum: 40 }, presence: true, uniqueness:true,
     format: { with: /\A[a-z0-9_]+\z/ }
+  validates :header_color, presence: true, format: {with: /\A#[0-9A-Fa-f]{3,6}\z/}
 
   has_many :questions, dependent: :delete_all
 
